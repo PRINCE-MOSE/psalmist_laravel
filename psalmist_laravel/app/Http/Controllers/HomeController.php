cb1b0f48e391b3b735993c20717c165282480049
PRINCE-MOSE <mosejnr.biz@gmail.com>
2024-03-02 22:27:21 +0300

    Added Models

--- psalmist_laravel/app/Http/Controllers/HomeController.php
+++ psalmist_laravel/app/Http/Controllers/HomeController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
 
     {
-        $allCategories=['Category 1','Category 2','Category 3q'];
+
+        $allCategories=Category::all();
         return view('index',['categories'=>$allCategories]);
     }
 }
